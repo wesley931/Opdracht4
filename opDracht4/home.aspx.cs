f054ee1775376df5ecdafe48f5b2c34c9c195e6d
wesley931 <mbarink03@student.rocvantwente.nl>
2017-03-20 14:32:50 +0100

    Verandere versie

--- opDracht4/home.aspx.cs
+++ opDracht4/home.aspx.cs
@@ -17,11 +17,11 @@ namespace opDracht4
             litVandaag.Text = dtVandaag;
             litTijd.Text = dtTijd;
 
-
+            //De datum van pasen(Daysleft) en de start datum (startdate)
             DateTime daysLeft = DateTime.Parse("16/4/2017 12:00:01 AM");
             DateTime startDate = DateTime.Now;
 
-
+            //Het verschil dat er komt
             TimeSpan timeLeft = daysLeft.Subtract(startDate);
 
             int uren = (timeLeft.Days * 24) + timeLeft.Hours;
